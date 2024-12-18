aafff58116dcbc62f4cc2808311d178c76d79cb9
Moafk Aljabi <mwafak_sh@hotmail.com>
2024-12-19 00:42:33 +0100

    started developing the c++ control part.

---
+++ c++/build/CMakeFiles/TCPServer.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/moafk/Documents/IoTControl/c++/src/control.cpp" "CMakeFiles/TCPServer.dir/src/control.cpp.o" "gcc" "CMakeFiles/TCPServer.dir/src/control.cpp.o.d"
+  "/home/moafk/Documents/IoTControl/c++/src/http_TCPServer.cpp" "CMakeFiles/TCPServer.dir/src/http_TCPServer.cpp.o" "gcc" "CMakeFiles/TCPServer.dir/src/http_TCPServer.cpp.o.d"
+  "/home/moafk/Documents/IoTControl/c++/src/main.cpp" "CMakeFiles/TCPServer.dir/src/main.cpp.o" "gcc" "CMakeFiles/TCPServer.dir/src/main.cpp.o.d"
+  "/home/moafk/Documents/IoTControl/c++/src/server.cpp" "CMakeFiles/TCPServer.dir/src/server.cpp.o" "gcc" "CMakeFiles/TCPServer.dir/src/server.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ c++/build/CMakeFiles/TCPServer.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TCPServer.dir/src/control.cpp.o"
+  "CMakeFiles/TCPServer.dir/src/control.cpp.o.d"
+  "CMakeFiles/TCPServer.dir/src/http_TCPServer.cpp.o"
+  "CMakeFiles/TCPServer.dir/src/http_TCPServer.cpp.o.d"
+  "CMakeFiles/TCPServer.dir/src/main.cpp.o"
+  "CMakeFiles/TCPServer.dir/src/main.cpp.o.d"
+  "CMakeFiles/TCPServer.dir/src/server.cpp.o"
+  "CMakeFiles/TCPServer.dir/src/server.cpp.o.d"
+  "TCPServer"
+  "TCPServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TCPServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
