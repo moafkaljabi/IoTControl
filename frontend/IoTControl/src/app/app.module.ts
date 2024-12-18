38e45855a73d203d50b64651dccc5fdd18262339
Moafk Aljabi <mwafak_sh@hotmail.com>
2024-12-18 13:53:38 +0100

    Added more frontend files

--- frontend/IoTControl/src/app/app.module.ts
+++ frontend/IoTControl/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { RouterModule } from '@angular/router';
 import { routes } from './app.routes';
+import { NgModule } from '@angular/core';
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
