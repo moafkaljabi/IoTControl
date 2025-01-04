caa8c868182233f108ab100e5ce3df4d899b2cca
Moafk Aljabi <mwafak_sh@hotmail.com>
2025-01-04 23:31:23 +0100

    Working on JSON in GO

--- backend/tcp_client/go/client.go
+++ backend/tcp_client/go/client.go
@@ -8,7 +8,6 @@ import (
 )
 
 func main() {
-	// Connect to the C++ server
 	conn, err := net.Dial("tcp", "0.0.0.0:8080")
 	if err != nil {
 		fmt.Println("Error connecting to server:", err)
@@ -18,11 +17,9 @@ func main() {
 
 	fmt.Println("Connected to the server. Type your message and press Enter:")
 
-	// Read input from the user
 	reader := bufio.NewReader(os.Stdin)
 	message, _ := reader.ReadString('\n')
 
-	// Send the message to the server
 	_, err = conn.Write([]byte(message))
 	if err != nil {
 		fmt.Println("Error sending message:", err)
