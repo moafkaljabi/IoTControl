82cde5432a207f3a2c4cded3c4d0f7d52de232d4
Moafk Aljabi <mwafak_sh@hotmail.com>
2025-03-10 00:56:46 +0100

    Migrating the UI into standalone only components

---
+++ frontend/IoTControl/IoTControl/src/app/signuo/signuo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signuo',
+  imports: [],
+  templateUrl: './signuo.component.html',
+  styleUrl: './signuo.component.css'
+})
+export class SignuoComponent {
+
+}
