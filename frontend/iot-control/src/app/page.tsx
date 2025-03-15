0dddca157f7b578a3a8cdc1a3dc68b173d694a31
Moafk Aljabi <mwafak_sh@hotmail.com>
2025-03-15 01:00:04 +0100

    Starting with Next from Angular

---
+++ frontend/iot-control/src/app/page.tsx
@@ -0,0 +1,9 @@
+// app/page.tsx
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to IoT Control</h1>
+      <p>This is your landing page.</p>
+    </div>
+  );
+}
---
+++ frontend/iot-control/src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+      <nav className="bg-blue-500 p-4 text-white">
+        <h1>Navbar</h1>
+      </nav>
+    );
+  }
+  
+  
